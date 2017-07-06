7281d4a7ec03a7005f9acf8c27931f07fa337029
elenasy <elenasy@users.noreply.github.com>
2017-07-06 07:23:03 -0300

    Update clustertags.py

--- clustertags.py
+++ clustertags.py
@@ -3,6 +3,9 @@ import numpy as np
 from scipy.cluster.vq import kmeans, kmeans2, whiten
 import matplotlib.pyplot as plt
 
+#read .csv file into a list 
+#Each element of this list is one row in the csv file
+
 with open('hashtagTime.csv', 'rb') as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quotechar='"')
     hashtag_list = []
@@ -11,19 +14,29 @@ with open('hashtagTime.csv', 'rb') as csvfile:
         print row
         hashtag_list.append(row)
 
+
+
+#Extract the epoch time and put them into a list for K-means clustering.
+
 data = np.ndarray(shape=(len(hashtag_list),1), dtype=float, order='F')
 for i in range(len(hashtag_list)):
     data[i] = hashtag_list[i][1]
 
-#print data
+print data
 
-#use k-means algorithm with given number of cluster centers
+
+
+#Use k-means algorithm with given number of cluster centers
+
 centers,idx = kmeans2(data,10)
 print centers
+
+
+#Plot
 
 zz = np.zeros((len(hashtag_list),1))
 nn = np.ones((len(centers),1))
 plt.figure(figsize=(10, 6), dpi=100)
-plt.scatter(data, zz, c=idx, s=100)
-plt.scatter(centers[:,0], nn, c='c', s=100)
+plt.scatter(data, zz, c=idx, s=20)
+plt.scatter(centers[:,0], nn, marker='v', s=80)
 plt.show()
